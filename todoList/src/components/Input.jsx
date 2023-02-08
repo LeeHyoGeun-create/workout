1ef56a55d26054d35b99d4ade5daea49f778656e
LeeHyoGeun-create <dkdkdk99835@gmail.com>
2023-02-08 11:19:23 +0900

    :sparkles: Input 기능 구현
    위도 경도를 바탕으로 날씨를 구하고 그를 바탕으로 사진을 받아와 데이터를 추가한다.

--- todoList/src/components/Input.jsx
+++ todoList/src/components/Input.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 import { StyledInputWrrap } from './styled/Input.styled';
+import { getPhoto } from '../module/api';
 
-const Input = () => {
+const Input = ({ weather, setData }) => {
   const [todo, setToDo] = useState('');
+  console.log(weather);
 
   const onInputChange = e => {
     setToDo(e.target.value);
+  };
+
+  const onClickButton = async () => {
+    const today = new Date();
+    const url = await getPhoto(weather);
+    const obj = { todo, url, date: today.toLocaleDateString('kr') };
+    setData(prev => [...prev, obj]);
   };
 
   return (
@@ -13,7 +22,7 @@ const Input = () => {
       <form>
         <input type="text" name="todo" value={todo} onChange={onInputChange} />
       </form>
-      <button>추가</button>
+      <button onClick={onClickButton}>추가</button>
     </StyledInputWrrap>
   );
 };
--- todoList/src/pages/Main.jsx
+++ todoList/src/pages/Main.jsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Input from '../components/Input';
+import initialData from '../assets/data.js';
+import { getWeather } from '../module/api';
 
 const Main = () => {
-  return <div>Hello</div>;
+  const [data, setData] = useState(initialData);
+  const [weather, setWeather] = useState('');
+
+  const success = async position => {
+    try {
+      const weather = await getWeather(
+        position.coords.latitude,
+        position.coords.longitude,
+      );
+      setWeather(weather);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const faile = async () => {
+    try {
+      const weather = await getWeather(37.5326, 127.024612);
+      setWeather(weather);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const setGeoLoaction = (success, faile) => {
+    if ('geolocation' in navigator) {
+      /* 위치정보 사용 가능 */
+      navigator.geolocation.getCurrentPosition(success);
+    } else {
+      faile();
+    }
+  };
+
+  useEffect(() => {
+    setGeoLoaction(success, faile);
+    console.log(data);
+  }, [data]);
+
+  return (
+    <main>
+      <Input weather={weather} setData={setData} />
+    </main>
+  );
 };
 
 export default Main;
